83017499b57282a57d1a87b3862058580a79a244
kousei yamashita <kyamashita@sei-corporation.co.jp>
2022-05-15 17:52:38 +0900

    update world

---
+++ app/Models/SandBox.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SandBox extends Model
+{
+    use HasFactory;
+}
--- resources/views/hello/world.blade.php
+++ resources/views/hello/world.blade.php
@@ -5,13 +5,25 @@ Hello World!
 1.
 コントローラの作成
 コマンド
+```
 php artisan make:controller SandBoxController --resource
+```
 雛形をapp/Http/Controllers/にひな形を作ってくれる。
 
 routing
-
+```
 use App\Http\Controllers\SandBoxController;
 Route::get('/sandbox', [SandBoxController::class, 'index']);
-
+```
 ※出なければrouteのキャッシュを削除。
 
+
+2.Model作成
+```
+php artisan make:model SandBox --migration
+```
+
+execute migration.
+```
+php artisan migrate
+```
